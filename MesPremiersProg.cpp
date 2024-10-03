7a60285de39486deda6b5e0f29f0edf3d4d0706e
Fabien Villedieu <fabien.villedieu.pro@gmail.com>
2024-10-03 11:50:33 +0200

    #3 Ajout du menu

--- MesPremiersProg.cpp
+++ MesPremiersProg.cpp
@@ -25,6 +25,45 @@ demande si on recommence le programme ou non avec lock
 while avec lock
 */
 int main(){
+    int lock=0;
+    do {
+        system("cls");
+        int choix;
+        std::cout << "Bienvenue sur le Menu Git de Programmes Informatiques\nVoici les programmes disponibles :\n1-Abonnement-Telephone\n2-Calcul de centimes\n3-Recherche de code ASCII\n4-Table ASCII\n5-Taille d'une variable\n6-Jeux de devinette\n";
+
+        do {
+            std::cout << "Merci de rentrer le numéro correspondant au programme voulu : ";
+            std::cin >> choix;
+        } while (choix != 1 && choix != 2 && choix != 3 && choix != 4 && choix != 5 && choix != 6);
+
+        if (choix == 1) {
+            std::cout << "Vous avez choisi : Abonnement-Telephone\n\n";
+            abonnement_tel();
+        }
+        if (choix == 2) {
+            std::cout << "Vous avez choisi : Calcul de centimes\n\n";
+            calcul_caisse();
+        }
+        if (choix == 3) {
+            std::cout << "Vous avez choisi : Recherche de code ASCII\n\n";
+            recherche_ASCII();
+        }
+        if (choix == 4) {
+            std::cout << "Vous avez choisi : Table ASCII\n\n";
+            table_ascii();
+        }
+        if (choix == 5) {
+            std::cout << "Vous avez choisi : Taille d'une variable\n\n";
+            taille_var();
+        }
+        if (choix == 6) {
+            std::cout << "Vous avez choisi : Jeux de devinette\n\n";
+            jeu_devi();
+        }
+        std::cout << "\n\nVoulez vous recommencez le programme ? (1=oui/2=non) : ";
+        std::cin >> lock;
+    } while (lock);
+
     return 0;
 }
 
@@ -33,28 +72,24 @@ int abonnement_tel() {
     float appelFA = 0.3, SMSFA = 0.1, appelFB = 0.2, SMSFB = 0.088, couttotalFA, couttotalFB, coutrelatif;
     int appelDuree, nmbrSMS;
     std::cout << "Bienvenue sur le calculateur de forfait telephonique de NightCall\n";
-
-    while (redo == 'O') {
-        std::cout << "Merci de nous donner la duree des appels fait par mois en minute : ";
-        std::cin >> appelDuree;
-        std::cout << "Merci de nous donner le nombre de SMS fait par mois : ";
-        std::cin >> nmbrSMS;
-        couttotalFA = appelDuree * appelFA + nmbrSMS * SMSFA + 2;
-        couttotalFB = appelDuree * appelFB + nmbrSMS * SMSFB + 4;
-        if (couttotalFA > couttotalFB) {
-            coutrelatif = couttotalFA / couttotalFB;
-        }
-        else {
-            coutrelatif = couttotalFB / couttotalFA;
-        }
-
-        std::cout << "Forfait A : " << couttotalFA << " euros\n";
-        std::cout << "Forfait B : " << couttotalFB << " euros\n";
-        std::cout << "Cout Relatif : " << coutrelatif << " euros\n";
-        std::cout << "Merci d'avoir utilise notre service !\n" << std::endl;
-        std::cout << "Voulez-vous refaire le test ? (O/N) ";
-        std::cin >> redo;
+    std::cout << "Merci de nous donner la duree des appels fait par mois en minute : ";
+    std::cin >> appelDuree;
+    std::cout << "Merci de nous donner le nombre de SMS fait par mois : ";
+    std::cin >> nmbrSMS;
+    couttotalFA = appelDuree * appelFA + nmbrSMS * SMSFA + 2;
+    couttotalFB = appelDuree * appelFB + nmbrSMS * SMSFB + 4;
+    if (couttotalFA > couttotalFB) {
+        coutrelatif = couttotalFA / couttotalFB;
     }
+    else {
+        coutrelatif = couttotalFB / couttotalFA;
+    }
+
+    std::cout << "Forfait A : " << couttotalFA << " euros\n";
+    std::cout << "Forfait B : " << couttotalFB << " euros\n";
+    std::cout << "Cout Relatif : " << coutrelatif << " euros\n";
+    std::cout << "Merci d'avoir utilise notre service !\n" << std::endl;
+    
     return 0;
 }
 
@@ -63,21 +98,21 @@ int calcul_caisse() {
     int nmbrE, nmbrC, totalE, totalC;
 
     std::cout << "Bienvenue dans le programme de somme des pieces !\n";
-    std::cout << "Donnez moi le nombre de pièces de 1 centimes : ";
+    std::cout << "Donnez moi le nombre de pieces de 1 centimes : ";
     std::cin >> c1;
-    std::cout << "Donnez moi le nombre de pièces de 2 centimes : ";
+    std::cout << "Donnez moi le nombre de pieces de 2 centimes : ";
     std::cin >> c2;
-    std::cout << "Donnez moi le nombre de pièces de 5 centimes : ";
+    std::cout << "Donnez moi le nombre de pieces de 5 centimes : ";
     std::cin >> c5;
-    std::cout << "Donnez moi le nombre de pièces de 10 centimes : ";
+    std::cout << "Donnez moi le nombre de pieces de 10 centimes : ";
     std::cin >> c10;
-    std::cout << "Donnez moi le nombre de pièces de 20 centimes : ";
+    std::cout << "Donnez moi le nombre de pieces de 20 centimes : ";
     std::cin >> c20;
-    std::cout << "Donnez moi le nombre de pièces de 50 centimes : ";
+    std::cout << "Donnez moi le nombre de pieces de 50 centimes : ";
     std::cin >> c50;
-    std::cout << "Donnez moi le nombre de pièces de 1 euros : ";
+    std::cout << "Donnez moi le nombre de pieces de 1 euros : ";
     std::cin >> e1;
-    std::cout << "Donnez moi le nombre de pièces de 2 euros : ";
+    std::cout << "Donnez moi le nombre de pieces de 2 euros : ";
     std::cin >> e2;
 
     nmbrE = e1 + e2 * 2;
